6725f5dc075d79224790bda8f24783915c514be2
aroy15 <anjonroy57@gmail.com>
2022-11-08 02:36:03 +0600

    mongodb connection successfully done

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
-
+require('dotenv').config()
 
 // middleware
 app.use(cors());
 app.use(express());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uyqo5y4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const serviceCollection = client.db('foodService').collection('foodItems');
+        const service = {
+            foodItem:'vagitable',
+            price:'30'
+        }
+        const result = await serviceCollection.insertOne(service)
+        console.log(`food item inserted ${result}`)
+    }
+    finally{
+
+    }
+}
+run().catch(err=>console.error(err))
+
+
 
 app.get('/',(req, res)=>{
     res.send('Food Delivery server is working')
