eff2082d837845a745b4ef19861eacddd7a86424
aroy15 <anjonroy57@gmail.com>
2022-11-10 23:33:33 +0600

    jwt updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -9,7 +10,21 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+      return  res.status(401).send({message: 'unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+           return res.status(401).send({message: 'unauthorized access'});
+        }
+        req.decoded = decoded;
+        next();
+    })
 
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uyqo5y4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,6 +32,15 @@ async function run() {
     try {
         const serviceCollection = client.db('foodService').collection('services');
         const reviewCollection = client.db('foodService').collection('reviews');
+        const blogCollection = client.db('foodService').collection('blogs');
+
+        // jwt token 
+        app.post('/jwt', (req, res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1d'});
+            res.send({token})
+        })
+
         // get data form database and send to client side
         app.get('/services', async (req, res) => {
             const limitRequest = parseInt(req.query.limit);
@@ -54,24 +78,38 @@ async function run() {
             res.send(result);
         })
 
-        // get reviews as per query: email & id
-        app.get('/reviews', async (req, res) => {
-            const requestId = req?.query?.id;
-            const requestEmail = req?.query?.email;
-            let query = '';
-            if (!requestId && !requestEmail) {
-                res.send('result not found')
-                return;
+        // get reviews as per query: email
+        app.get('/reviews', verifyJWT, async (req, res) => {
+
+            const decoded = req.decoded;
+            let query = {};
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({message: 'unauthorized access'});
             }
-            else if (requestId) {
-                query = { serviceId: requestId };
-            } else if (requestEmail) {
-                query = { email: requestEmail }
+           
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
             }
             const cursor = reviewCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
+
+         // get reviews for publicly:
+         app.get('/reviews-public', async (req, res) => {
+            const requestId = req?.query?.id;
+            if (!requestId) {
+                res.send('result not found')
+                return;
+            }
+            const query = { serviceId: requestId };
+            const cursor = reviewCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
 
         // Get single review
         app.get('/reviews/:id', async (req, res) => {
@@ -101,6 +139,14 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await reviewCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        // Get Blog Content
+        app.get('/blogs', async (req, res) => {
+            const query = {};
+            const cursor = blogCollection.find(query);
+            const result = await cursor.toArray()
             res.send(result)
         })
     }
