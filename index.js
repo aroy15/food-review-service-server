cc65166c85a334c0c2c0b6e5753e027ee7155e4a
aroy15 <anjonroy57@gmail.com>
2022-11-10 15:26:00 +0600

    patch method applied for updating reviews

--- index.js
+++ index.js
@@ -7,50 +7,100 @@ require('dotenv').config()
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uyqo5y4.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
+async function run() {
+    try {
         const serviceCollection = client.db('foodService').collection('services');
+        const reviewCollection = client.db('foodService').collection('reviews');
         // get data form database and send to client side
-        app.get('/services', async(req, res)=>{
+        app.get('/services', async (req, res) => {
             const limitRequest = parseInt(req.query.limit);
             const query = {};
             const cursor = serviceCollection.find(query);
             let result = '';
-            if(limitRequest){
+            if (limitRequest) {
                 result = await cursor.limit(limitRequest).toArray()
-            }else{
-                result =  await cursor.toArray()
+            } else {
+                result = await cursor.toArray()
             }
             res.send(result)
         })
 
         // get single data
-        app.get('/services/:id', async(req, res)=>{
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await serviceCollection.findOne(query);
             res.send(result)
         })
+
+        // Add Reviews on the Database
+        app.post('/reviews', async (req, res) => {
+            const reviewItem = req.body;
+            const result = await reviewCollection.insertOne(reviewItem);
+            res.send(result);
+        })
+
+        // get reviews form data base
+        app.get('/reviews', async (req, res) => {
+            const requestId = req?.query?.id;
+            const requestEmail = req?.query?.email;
+            let query = '';
+            if (!requestId && !requestEmail) {
+                res.send('result not found')
+                return;
+            }
+            else if (requestId) {
+                query = { serviceId: requestId };
+            } else if (requestEmail) {
+                query = { email: requestEmail }
+            }
+            const cursor = reviewCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await reviewCollection.findOne(query);
+            res.send(result)
+        })
+
+        // update review
+        app.patch('/reviews/:id', async (req, res)=>{
+            const id = req.params.id;
+            const updatedReview =  req.body;
+            const query = {_id: ObjectId(id)}
+            const updatedDoc = {
+                $set:{
+                    rating:updatedReview.rating,
+                    message:updatedReview.message
+                }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc)
+            res.send(result)
+        })
+
     }
-    finally{
+    finally {
 
     }
 }
-run().catch(err=>console.error(err))
+run().catch(err => console.error(err))
 
 
 
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('Food Delivery server is working')
 })
 
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Food Delivery server is running on ${port}`)
 })
